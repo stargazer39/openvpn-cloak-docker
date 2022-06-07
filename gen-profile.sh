ca4ff3bf0ed13c4825c5da86b61d0a4a6d163a85
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-06-07 11:46:20 +0000

    Fixed OpenVPN bug

--- gen-profile.sh
+++ gen-profile.sh
@@ -17,4 +17,7 @@ fi
 
 docker run -v $DATA:/etc/openvpn --rm -it $REPO_NAME easyrsa build-client-full $1 nopass
 docker run -v $DATA:/etc/openvpn --rm $REPO_NAME ovpn_getclient $1 > $PROFILES/$1.ovpn
+
 echo "route $HOST 255.255.255.255 net_gateway" >> $PROFILES/$1.ovpn
+echo "ignore-unknown-option block-outside-dns" >> $PROFILES/$1.ovpn
+echo "setenv opt block-outside-dns" >> $PROFILES/$1.ovpn
