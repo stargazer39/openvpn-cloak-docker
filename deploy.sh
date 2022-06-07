798639c8df425a9d2be0128bc97f6bcb925e3c6a
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-06-07 09:06:53 +0000

    New docker install script

--- deploy.sh
+++ deploy.sh
@@ -13,18 +13,11 @@ HOST=$(curl checkip.amazonaws.com)
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install zip unzip git ufw
-apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get -y update 
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
 apt-get -y install docker-compose 
 systemctl enable docker --now
 
